277409fbf1d6db299e951336499a720c07dad1c0
Bravista <sekretz@web.de>
2024-05-21 15:16:03 +0200

    initialisieren

---
+++ lib/src/features/dirty_screen_1/presentation/box_number.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class BoxNumber extends StatelessWidget {
+  final String number;
+
+  const BoxNumber({super.key, required this.number});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12), color: Colors.redAccent),
+      margin: const EdgeInsets.all(4),
+      padding: const EdgeInsets.all(16.0),
+      child: Row(
+        children: [
+          const Icon(Icons.bug_report),
+          const SizedBox(
+            width: 8,
+          ),
+          Text(
+            'Box #$number',
+            style: const TextStyle(fontSize: 20, color: Colors.white),
+          )
+        ],
+      ),
+    );
+  }
+}
